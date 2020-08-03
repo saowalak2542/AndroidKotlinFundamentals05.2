0893b152ba997e3fb6fcd60e6d871285cf58cbe6
saowalak.tb@rmuti.ac.th <b0613384510>
2020-08-04 03:39:45 +0700

    05.2-9

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -24,6 +24,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
@@ -56,6 +57,16 @@ class ScoreFragment : Fragment() {
             binding.scoreText.text = viewModel.score.toString()
         })
         binding.scoreText.text = viewModel.score.toString()
+        binding.playAgainButton.setOnClickListener {  viewModel.onPlayAgain()  }
+
+        // Navigates back to game when button is pressed
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
+            if (playAgain) {
+                findNavController().navigate(ScoreFragmentDirections.actionRestart())
+                viewModel.onPlayAgainComplete()
+            }
+        })
+
         return binding.root
     }
 }
